c3a5480c2151ae807534e1d85bd1b471773eaaa6
themathematcs <chriznganga69@gmail.com>
2025-12-08 01:45:12 +0300

    Updates

--- assets/Certifications-BXv2z2PQ.js
+++ assets/Certifications-BXv2z2PQ.js
@@ -1,4 +1,4 @@
-import{c as o,j as e,m as s,C as i}from"./index-CtSwZe--.js";/**
+import{c as o,j as e,m as s,C as i}from"./index-rYxvdvFH.js";/**
  * @license lucide-react v0.556.0 - ISC
  *
  * This source code is licensed under the ISC license.
