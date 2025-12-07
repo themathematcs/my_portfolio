5fbdac18f10844d7b8f298fafad7a5e80a1abc2d
themathematcs <chriznganga69@gmail.com>
2025-12-07 23:55:21 +0300

    Updates

--- assets/Certifications-D_1G6OWm.js
+++ assets/Certifications-D_1G6OWm.js
@@ -1,4 +1,4 @@
-import{c as o,j as e,m as s,C as i}from"./index-hFgrfdQ2.js";/**
+import{c as o,j as e,m as s,C as i}from"./index-DsnBwbp8.js";/**
  * @license lucide-react v0.556.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/Projects-BBwvZyc3.js
+++ assets/Projects-BBwvZyc3.js
@@ -1,4 +1,4 @@
-import{c as i,j as e,m as t,P as l}from"./index-hFgrfdQ2.js";/**
+import{c as i,j as e,m as t,P as l}from"./index-DsnBwbp8.js";/**
  * @license lucide-react v0.556.0 - ISC
  *
  * This source code is licensed under the ISC license.
