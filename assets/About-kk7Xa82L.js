b923f4f3128d292d7fa415cf573435db02904806
themathematcs <chriznganga69@gmail.com>
2025-12-08 03:04:18 +0300

    Updates

--- assets/About-kk7Xa82L.js
+++ assets/About-kk7Xa82L.js
@@ -1,4 +1,4 @@
-import{c as a,j as e,m as s,A as l}from"./index-CWg07lwI.js";/**
+import{c as a,j as e,m as s,A as l}from"./index-C0YcIzZJ.js";/**
  * @license lucide-react v0.556.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/Projects-Bjx0L3ov.js
+++ assets/Projects-Bjx0L3ov.js
@@ -1,4 +1,4 @@
-import{c as i,j as e,m as t,P as l}from"./index-CWg07lwI.js";/**
+import{c as i,j as e,m as t,P as l}from"./index-C0YcIzZJ.js";/**
  * @license lucide-react v0.556.0 - ISC
  *
  * This source code is licensed under the ISC license.
